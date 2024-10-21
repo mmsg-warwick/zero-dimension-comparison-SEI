349928cd6bbcad43f48e788f616b77762aa964fe
kawaMANMI <kawamam2007@gmail.com>
2024-10-21 12:04:37 +0100

    cleaning code

--- examples/style_figs.py
+++ examples/style_figs.py
@@ -2,22 +2,7 @@ import matplotlib.pyplot as plt
 
 
 def set_plotting_format(mode):
-    # color_blind = plt.cm.cividis(np.linspace(0, 1, 4))
-    # color_blind = plt.cm.get_cmap('tableau-colorblind10').colors[:4]
-    # plt.style.use(["science"])
-    # plt.rcParams["axes.prop_cycle"] = plt.cycler(color=color_blind)
-    #
-    # color_blind_friendly = [
-    #     '#006BA4', '#FF800E', '#ABABAB', '#595959',
-    #     '#5F9ED1', '#C85200', '#898989', '#A2C8EC',
-    #     '#FFBC79', '#CFCFCF'
-    # ]
-
-    # Use the science style
     plt.style.use(["science", 'muted'])
-
-    # Set the color cycle
-    # plt.rcParams["axes.prop_cycle"] = plt.cycler(color=color_blind_friendly)
 
     if mode == "presentation":
         plt.rcParams.update(
